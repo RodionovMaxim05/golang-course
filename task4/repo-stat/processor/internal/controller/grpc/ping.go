37b0d5ab8dcd288e19dcc8cbfa0fff33b38ed564
Maxim Rodionov <rodionov.maxim05@gmail.com>
2026-04-18 18:45:50 +0300

    refactor: fix linter coms

--- task4/repo-stat/processor/internal/controller/grpc/ping.go
+++ task4/repo-stat/processor/internal/controller/grpc/ping.go
@@ -10,17 +10,17 @@ import (
 
 type Server struct {
 	processorpb.UnimplementedProcessorServer
-	log                *slog.Logger
-	ping               *usecase.Ping
-	getRepo            *usecase.GetRepo
+	log                  *slog.Logger
+	ping                 *usecase.Ping
+	getRepo              *usecase.GetRepo
 	getSubscriptionsInfo *usecase.GetSubscriptionsInfo
 }
 
 func NewServer(log *slog.Logger, ping *usecase.Ping, getRepo *usecase.GetRepo, getSubscriptionsInfo *usecase.GetSubscriptionsInfo) *Server {
 	return &Server{
-		log:                log,
-		ping:               ping,
-		getRepo:            getRepo,
+		log:                  log,
+		ping:                 ping,
+		getRepo:              getRepo,
 		getSubscriptionsInfo: getSubscriptionsInfo,
 	}
 }
