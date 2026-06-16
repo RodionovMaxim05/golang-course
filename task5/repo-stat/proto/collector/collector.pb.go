4a82496cbeddc369932d7bfdc26c808c3d87d606
Maxim Rodionov <rodionov.maxim05@gmail.com>
2026-06-16 15:33:48 +0300

    refactor: move common proto-structures into a common file

--- task5/repo-stat/proto/collector/collector.pb.go
+++ task5/repo-stat/proto/collector/collector.pb.go
@@ -9,7 +9,7 @@ package collectorv1
 import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
-	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
+	_ "google.golang.org/protobuf/types/known/timestamppb"
 	reflect "reflect"
 	sync "sync"
 )
@@ -76,170 +76,6 @@ func (x *GetRepoRequest) GetRepo() string {
 	return ""
 }
 
-type GetRepoResponse struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	FullName        string                 `protobuf:"bytes,1,opt,name=full_name,json=fullName,proto3" json:"full_name,omitempty"`
-	Description     string                 `protobuf:"bytes,2,opt,name=description,proto3" json:"description,omitempty"`
-	StargazersCount int32                  `protobuf:"varint,3,opt,name=stargazers_count,json=stargazersCount,proto3" json:"stargazers_count,omitempty"`
-	ForksCount      int32                  `protobuf:"varint,4,opt,name=forks_count,json=forksCount,proto3" json:"forks_count,omitempty"`
-	CreatedAt       *timestamppb.Timestamp `protobuf:"bytes,5,opt,name=created_at,json=createdAt,proto3" json:"created_at,omitempty"`
-}
-
-func (x *GetRepoResponse) Reset() {
-	*x = GetRepoResponse{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_proto_collector_collector_proto_msgTypes[1]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *GetRepoResponse) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*GetRepoResponse) ProtoMessage() {}
-
-func (x *GetRepoResponse) ProtoReflect() protoreflect.Message {
-	mi := &file_proto_collector_collector_proto_msgTypes[1]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use GetRepoResponse.ProtoReflect.Descriptor instead.
-func (*GetRepoResponse) Descriptor() ([]byte, []int) {
-	return file_proto_collector_collector_proto_rawDescGZIP(), []int{1}
-}
-
-func (x *GetRepoResponse) GetFullName() string {
-	if x != nil {
-		return x.FullName
-	}
-	return ""
-}
-
-func (x *GetRepoResponse) GetDescription() string {
-	if x != nil {
-		return x.Description
-	}
-	return ""
-}
-
-func (x *GetRepoResponse) GetStargazersCount() int32 {
-	if x != nil {
-		return x.StargazersCount
-	}
-	return 0
-}
-
-func (x *GetRepoResponse) GetForksCount() int32 {
-	if x != nil {
-		return x.ForksCount
-	}
-	return 0
-}
-
-func (x *GetRepoResponse) GetCreatedAt() *timestamppb.Timestamp {
-	if x != nil {
-		return x.CreatedAt
-	}
-	return nil
-}
-
-type GetSubsInfoRequest struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-}
-
-func (x *GetSubsInfoRequest) Reset() {
-	*x = GetSubsInfoRequest{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_proto_collector_collector_proto_msgTypes[2]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *GetSubsInfoRequest) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*GetSubsInfoRequest) ProtoMessage() {}
-
-func (x *GetSubsInfoRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_proto_collector_collector_proto_msgTypes[2]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use GetSubsInfoRequest.ProtoReflect.Descriptor instead.
-func (*GetSubsInfoRequest) Descriptor() ([]byte, []int) {
-	return file_proto_collector_collector_proto_rawDescGZIP(), []int{2}
-}
-
-type GetSubsInfoResponse struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	Repositories []*GetRepoResponse `protobuf:"bytes,1,rep,name=repositories,proto3" json:"repositories,omitempty"`
-}
-
-func (x *GetSubsInfoResponse) Reset() {
-	*x = GetSubsInfoResponse{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_proto_collector_collector_proto_msgTypes[3]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *GetSubsInfoResponse) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*GetSubsInfoResponse) ProtoMessage() {}
-
-func (x *GetSubsInfoResponse) ProtoReflect() protoreflect.Message {
-	mi := &file_proto_collector_collector_proto_msgTypes[3]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use GetSubsInfoResponse.ProtoReflect.Descriptor instead.
-func (*GetSubsInfoResponse) Descriptor() ([]byte, []int) {
-	return file_proto_collector_collector_proto_rawDescGZIP(), []int{3}
-}
-
-func (x *GetSubsInfoResponse) GetRepositories() []*GetRepoResponse {
-	if x != nil {
-		return x.Repositories
-	}
-	return nil
-}
-
 var File_proto_collector_collector_proto protoreflect.FileDescriptor
 
 var file_proto_collector_collector_proto_rawDesc = []byte{
@@ -251,30 +87,10 @@ var file_proto_collector_collector_proto_rawDesc = []byte{
 	0x70, 0x6f, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x14, 0x0a, 0x05, 0x6f, 0x77, 0x6e,
 	0x65, 0x72, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x6f, 0x77, 0x6e, 0x65, 0x72, 0x12,
 	0x12, 0x0a, 0x04, 0x72, 0x65, 0x70, 0x6f, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x72,
-	0x65, 0x70, 0x6f, 0x22, 0xd7, 0x01, 0x0a, 0x0f, 0x47, 0x65, 0x74, 0x52, 0x65, 0x70, 0x6f, 0x52,
-	0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x1b, 0x0a, 0x09, 0x66, 0x75, 0x6c, 0x6c, 0x5f,
-	0x6e, 0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x66, 0x75, 0x6c, 0x6c,
-	0x4e, 0x61, 0x6d, 0x65, 0x12, 0x20, 0x0a, 0x0b, 0x64, 0x65, 0x73, 0x63, 0x72, 0x69, 0x70, 0x74,
-	0x69, 0x6f, 0x6e, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0b, 0x64, 0x65, 0x73, 0x63, 0x72,
-	0x69, 0x70, 0x74, 0x69, 0x6f, 0x6e, 0x12, 0x29, 0x0a, 0x10, 0x73, 0x74, 0x61, 0x72, 0x67, 0x61,
-	0x7a, 0x65, 0x72, 0x73, 0x5f, 0x63, 0x6f, 0x75, 0x6e, 0x74, 0x18, 0x03, 0x20, 0x01, 0x28, 0x05,
-	0x52, 0x0f, 0x73, 0x74, 0x61, 0x72, 0x67, 0x61, 0x7a, 0x65, 0x72, 0x73, 0x43, 0x6f, 0x75, 0x6e,
-	0x74, 0x12, 0x1f, 0x0a, 0x0b, 0x66, 0x6f, 0x72, 0x6b, 0x73, 0x5f, 0x63, 0x6f, 0x75, 0x6e, 0x74,
-	0x18, 0x04, 0x20, 0x01, 0x28, 0x05, 0x52, 0x0a, 0x66, 0x6f, 0x72, 0x6b, 0x73, 0x43, 0x6f, 0x75,
-	0x6e, 0x74, 0x12, 0x39, 0x0a, 0x0a, 0x63, 0x72, 0x65, 0x61, 0x74, 0x65, 0x64, 0x5f, 0x61, 0x74,
-	0x18, 0x05, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x1a, 0x2e, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2e,
-	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2e, 0x54, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61,
-	0x6d, 0x70, 0x52, 0x09, 0x63, 0x72, 0x65, 0x61, 0x74, 0x65, 0x64, 0x41, 0x74, 0x22, 0x14, 0x0a,
-	0x12, 0x47, 0x65, 0x74, 0x53, 0x75, 0x62, 0x73, 0x49, 0x6e, 0x66, 0x6f, 0x52, 0x65, 0x71, 0x75,
-	0x65, 0x73, 0x74, 0x22, 0x4f, 0x0a, 0x13, 0x47, 0x65, 0x74, 0x53, 0x75, 0x62, 0x73, 0x49, 0x6e,
-	0x66, 0x6f, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x38, 0x0a, 0x0c, 0x72, 0x65,
-	0x70, 0x6f, 0x73, 0x69, 0x74, 0x6f, 0x72, 0x69, 0x65, 0x73, 0x18, 0x01, 0x20, 0x03, 0x28, 0x0b,
-	0x32, 0x14, 0x2e, 0x61, 0x70, 0x69, 0x2e, 0x47, 0x65, 0x74, 0x52, 0x65, 0x70, 0x6f, 0x52, 0x65,
-	0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x52, 0x0c, 0x72, 0x65, 0x70, 0x6f, 0x73, 0x69, 0x74, 0x6f,
-	0x72, 0x69, 0x65, 0x73, 0x42, 0x31, 0x5a, 0x2f, 0x72, 0x65, 0x70, 0x6f, 0x2d, 0x73, 0x74, 0x61,
-	0x74, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x67, 0x65, 0x6e, 0x2f, 0x67, 0x6f, 0x2f, 0x63,
-	0x6f, 0x6c, 0x6c, 0x65, 0x63, 0x74, 0x6f, 0x72, 0x2f, 0x76, 0x31, 0x3b, 0x63, 0x6f, 0x6c, 0x6c,
-	0x65, 0x63, 0x74, 0x6f, 0x72, 0x76, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x65, 0x70, 0x6f, 0x42, 0x31, 0x5a, 0x2f, 0x72, 0x65, 0x70, 0x6f, 0x2d, 0x73, 0x74, 0x61, 0x74,
+	0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x67, 0x65, 0x6e, 0x2f, 0x67, 0x6f, 0x2f, 0x63, 0x6f,
+	0x6c, 0x6c, 0x65, 0x63, 0x74, 0x6f, 0x72, 0x2f, 0x76, 0x31, 0x3b, 0x63, 0x6f, 0x6c, 0x6c, 0x65,
+	0x63, 0x74, 0x6f, 0x72, 0x76, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -289,22 +105,16 @@ func file_proto_collector_collector_proto_rawDescGZIP() []byte {
 	return file_proto_collector_collector_proto_rawDescData
 }
 
-var file_proto_collector_collector_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
+var file_proto_collector_collector_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
 var file_proto_collector_collector_proto_goTypes = []interface{}{
-	(*GetRepoRequest)(nil),        // 0: api.GetRepoRequest
-	(*GetRepoResponse)(nil),       // 1: api.GetRepoResponse
-	(*GetSubsInfoRequest)(nil),    // 2: api.GetSubsInfoRequest
-	(*GetSubsInfoResponse)(nil),   // 3: api.GetSubsInfoResponse
-	(*timestamppb.Timestamp)(nil), // 4: google.protobuf.Timestamp
+	(*GetRepoRequest)(nil), // 0: api.GetRepoRequest
 }
 var file_proto_collector_collector_proto_depIdxs = []int32{
-	4, // 0: api.GetRepoResponse.created_at:type_name -> google.protobuf.Timestamp
-	1, // 1: api.GetSubsInfoResponse.repositories:type_name -> api.GetRepoResponse
-	2, // [2:2] is the sub-list for method output_type
-	2, // [2:2] is the sub-list for method input_type
-	2, // [2:2] is the sub-list for extension type_name
-	2, // [2:2] is the sub-list for extension extendee
-	0, // [0:2] is the sub-list for field type_name
+	0, // [0:0] is the sub-list for method output_type
+	0, // [0:0] is the sub-list for method input_type
+	0, // [0:0] is the sub-list for extension type_name
+	0, // [0:0] is the sub-list for extension extendee
+	0, // [0:0] is the sub-list for field type_name
 }
 
 func init() { file_proto_collector_collector_proto_init() }
@@ -325,42 +135,6 @@ func file_proto_collector_collector_proto_init() {
 				return nil
 			}
 		}
-		file_proto_collector_collector_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*GetRepoResponse); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
-		file_proto_collector_collector_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*GetSubsInfoRequest); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
-		file_proto_collector_collector_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*GetSubsInfoResponse); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -368,7 +142,7 @@ func file_proto_collector_collector_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_proto_collector_collector_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   4,
+			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
