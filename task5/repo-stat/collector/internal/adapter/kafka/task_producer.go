af13bc04b186c56f93ff1769b70629256735e756
Maxim Rodionov <rodionov.maxim05@gmail.com>
2026-06-16 15:11:53 +0300

    feat: make Collector send update task to Kafka

---
+++ task5/repo-stat/collector/internal/adapter/kafka/task_producer.go
@@ -0,0 +1,58 @@
+package kafka
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/segmentio/kafka-go"
+	"google.golang.org/protobuf/proto"
+
+	"repo-stat/collector/config"
+	collectorpb "repo-stat/proto/collector"
+)
+
+type TaskProducerAdapter struct {
+	log    *slog.Logger
+	writer *kafka.Writer
+}
+
+func NewTaskProducerAdapter(cfg config.Kafka, log *slog.Logger) *TaskProducerAdapter {
+	return &TaskProducerAdapter{
+		log: log,
+		writer: &kafka.Writer{
+			Addr:         kafka.TCP(cfg.Address),
+			Topic:        cfg.ConsumerTopic,
+			Balancer:     &kafka.LeastBytes{},
+			RequiredAcks: kafka.RequireOne,
+		},
+	}
+}
+
+func (tpa *TaskProducerAdapter) SendCollectionTask(ctx context.Context, owner, repo string) error {
+	getRepoRequest := &collectorpb.GetRepoRequest{
+		Owner: owner,
+		Repo:  repo,
+	}
+
+	tpa.log.Debug("task producer get request")
+
+	requestBytes, err := proto.Marshal(getRepoRequest)
+	if err != nil {
+		tpa.log.Error("failed to Marshal get repo request", "error", err)
+		return err
+	}
+
+	err = tpa.writer.WriteMessages(ctx, kafka.Message{Value: requestBytes})
+	if err != nil {
+		tpa.log.Error("failed to write messages in Kafka", "error", err)
+		return err
+	}
+
+	tpa.log.Debug("task producer send request", "owner", owner, "repo", repo)
+
+	return nil
+}
+
+func (tpa *TaskProducerAdapter) Close() error {
+	return tpa.writer.Close()
+}
--- task5/repo-stat/collector/internal/usecase/get_subscriptions_info.go
+++ task5/repo-stat/collector/internal/usecase/get_subscriptions_info.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"context"
+	"log/slog"
 
 	"repo-stat/collector/internal/domain"
 )
@@ -10,25 +11,22 @@ type SubscriberClient interface {
 	GetSubscriptions(ctx context.Context) ([]domain.Subscription, error)
 }
 
-type GitHubClient interface {
-	GetRepo(ctx context.Context, owner, repo string) (domain.Repository, error)
-}
-
-type KafkaResultProducer interface {
-	SendRepoResult(ctx context.Context, repo domain.Repository) error
+type TaskProducer interface {
+	SendCollectionTask(ctx context.Context, owner, repo string) error
 }
 
 type GetSubscriptionsInfoUsecase struct {
+	log              *slog.Logger
 	subscriberClient SubscriberClient
-	githubClient     GitHubClient
-	kafkaProducer    KafkaResultProducer
+	taskProducer     TaskProducer
 }
 
-func NewGetSubscriptionsInfoUsecase(subscriberClient SubscriberClient, githubClient GitHubClient, kafkaProducer KafkaResultProducer) *GetSubscriptionsInfoUsecase {
+func NewGetSubscriptionsInfoUsecase(log *slog.Logger, subscriberClient SubscriberClient, taskProducer TaskProducer,
+) *GetSubscriptionsInfoUsecase {
 	return &GetSubscriptionsInfoUsecase{
+		log:              log,
 		subscriberClient: subscriberClient,
-		githubClient:     githubClient,
-		kafkaProducer:    kafkaProducer,
+		taskProducer:     taskProducer,
 	}
 }
 
@@ -36,19 +34,19 @@ func (gsiu *GetSubscriptionsInfoUsecase) Execute(ctx context.Context) error {
 	// Get subscriptions from subscriber service
 	subscriptions, err := gsiu.subscriberClient.GetSubscriptions(ctx)
 	if err != nil {
+		gsiu.log.Error("failed to get subscriptions from subscriber client", "error", err)
 		return err
 	}
 
+	// Submit a request to receive subscription data
 	for _, sub := range subscriptions {
-		// Get repository info for each subscription
-		repo, err := gsiu.githubClient.GetRepo(ctx, sub.Owner, sub.Repo)
+		err = gsiu.taskProducer.SendCollectionTask(ctx, sub.Owner, sub.Repo)
 		if err != nil {
-			continue
-		}
-
-		// Send the finished result to Kafka
-		err = gsiu.kafkaProducer.SendRepoResult(ctx, repo)
-		if err != nil {
+			gsiu.log.Error("failed to publish collection task to producer",
+				"owner", sub.Owner,
+				"repo", sub.Repo,
+				"error", err,
+			)
 			continue
 		}
 	}
